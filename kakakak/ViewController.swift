b51764387d54b2c96731b038ed2a91a383fa7b89
KangSungYong <jongwon676@naver.com>
2019-01-06 21:03:33 +0900

    Initial Commit

---
+++ kakakak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kakakak
+//
+//  Created by 성용강 on 06/01/2019.
+//  Copyright © 2019 성용강. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
