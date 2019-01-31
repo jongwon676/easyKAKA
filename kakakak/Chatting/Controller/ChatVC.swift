3767872a1ed2be528ef53f450b6d71a7e197abe8
KangSungYong <jongwon676@naver.com>
2019-02-01 03:43:39 +0900

    asdfasdfasdf

--- kakakak/Chatting/Controller/ChatVC.swift
+++ kakakak/Chatting/Controller/ChatVC.swift
@@ -322,7 +322,7 @@ extension ChatVC: UITableViewDataSource,UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         print(indexPath.row)
         
-        let cell2 = tableView.dequeueReusableCell(withIdentifier: "deleteMessageAnother")
+        let cell2 = tableView.dequeueReusableCell(withIdentifier: "recordMessageAnother")
         return cell2!
         let msg = messageManager.getMessage(idx: indexPath.row)
         let cell = tableView.dequeueReusableCell(withIdentifier: msg.type.rawValue)
