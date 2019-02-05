a19aab27f74cb37d1ea60f877576f7b9f56dddc6
KangSungYong <jongwon676@naver.com>
2019-02-05 18:19:09 +0900

    asdfasdfadfadf

--- kakakak/RoomList/RoomListVC.swift
+++ kakakak/RoomList/RoomListVC.swift
@@ -8,6 +8,7 @@ class RoomListVC: UIViewController,UITableViewDataSource,UITableViewDelegate{
     var bannerView: GADBannerView = GADBannerView(frame:  CGRect(x: 0, y: 0, width: 200, height: 200))
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
         rooms = Room.all()
 //        tableView.delegate = self
 //        tableView.rowHeight = 77
@@ -28,8 +29,14 @@ class RoomListVC: UIViewController,UITableViewDataSource,UITableViewDelegate{
             tableView.reloadData()
         }
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        addButton.isHidden = true
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+       addButton.isHidden = false
         navigationController?.navigationBar.barTintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         self.tabBarController?.tabBar.isHidden = false
         tableView.reloadData()
@@ -70,5 +77,53 @@ class RoomListVC: UIViewController,UITableViewDataSource,UITableViewDelegate{
         }
     }
     
+    
+    
+    /// WARNING: Change these constants according to your project's design
+    private struct Const {
+        /// Image height/width for Large NavBar state
+        static let ImageSizeForLargeState: CGFloat = 20
+        /// Margin from right anchor of safe area to right anchor of Image
+        static let ImageRightMargin: CGFloat = 16
+        /// Margin from bottom anchor of NavBar to bottom anchor of Image for Large NavBar state
+        static let ImageBottomMarginForLargeState: CGFloat = 12
+        /// Margin from bottom anchor of NavBar to bottom anchor of Image for Small NavBar state
+        static let ImageBottomMarginForSmallState: CGFloat = 6
+        /// Image height/width for Small NavBar state
+        static let ImageSizeForSmallState: CGFloat = 20
+        /// Height of NavBar for Small state. Usually it's just 44
+        static let NavBarHeightSmallState: CGFloat = 44
+        /// Height of NavBar for Large state. Usually it's just 96.5 but if you have a custom font for the title, please make sure to edit this value since it changes the height for Large state of NavBar
+        static let NavBarHeightLargeState: CGFloat = 96.5
+    }
+    
+    var addButton = UIButton(type: .custom)
+    private func setupUI() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        
+        // Initial setup for image for Large NavBar state since the the screen always has Large NavBar once it gets opened
+        guard let navigationBar = self.navigationController?.navigationBar else { return }
+        navigationBar.addSubview(addButton)
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+        addButton.setImage(UIImage(named: "addBtn"), for: .normal)
+        addButton.addTarget(self, action: #selector(addBtnClicked(_:)), for: .touchUpInside)
+        
+        
+        NSLayoutConstraint.activate([
+            addButton.rightAnchor.constraint(equalTo: navigationBar.rightAnchor, constant: -Const.ImageRightMargin),
+            addButton.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: -Const.ImageBottomMarginForLargeState),
+            addButton.heightAnchor.constraint(equalToConstant: Const.ImageSizeForLargeState),
+            addButton.widthAnchor.constraint(equalTo: addButton.heightAnchor)
+            ])
+    }
+    
+    
+    @objc func addBtnClicked(_ sender: UIButton){
+        if let roomAddVC = storyboard?.instantiateViewController(withIdentifier: "RoomAddVC") as? RoomAddVC{
+            self.present(roomAddVC, animated: true, completion: nil)
+
+        }
+    }
     
 }
--- kakakak/UserList/CharacterListVC.swift
+++ kakakak/UserList/CharacterListVC.swift
@@ -12,10 +12,12 @@ class CharacterListVC: UITableViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tableView.rowHeight = 70
         
+        setupUI()
         presets = Preset.all()
     }
+    
+    
     
     
     override func viewWillAppear(_ animated: Bool) {
@@ -32,12 +34,13 @@ class CharacterListVC: UITableViewController{
             case .error: break
             }
         }
+        addButton.isHidden = false
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         token?.invalidate()
-        
+        addButton.isHidden = true
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -58,6 +61,12 @@ class CharacterListVC: UITableViewController{
     }
     
     
+    @objc func addBtnClicked(_ sender: UIButton){
+        if let editUserVc = storyboard?.instantiateViewController(withIdentifier: "AddUserController") as? AddUserController{
+            
+            self.navigationController?.pushViewController(editUserVc, animated: true)
+        }
+    }
     
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -66,4 +75,48 @@ class CharacterListVC: UITableViewController{
             self.navigationController?.pushViewController(editUserVc, animated: true)
         }
     }
+    
+    
+    
+    /// WARNING: Change these constants according to your project's design
+    private struct Const {
+        /// Image height/width for Large NavBar state
+        static let ImageSizeForLargeState: CGFloat = 20
+        /// Margin from right anchor of safe area to right anchor of Image
+        static let ImageRightMargin: CGFloat = 16
+        /// Margin from bottom anchor of NavBar to bottom anchor of Image for Large NavBar state
+        static let ImageBottomMarginForLargeState: CGFloat = 12
+        /// Margin from bottom anchor of NavBar to bottom anchor of Image for Small NavBar state
+        static let ImageBottomMarginForSmallState: CGFloat = 6
+        /// Image height/width for Small NavBar state
+        static let ImageSizeForSmallState: CGFloat = 20
+        /// Height of NavBar for Small state. Usually it's just 44
+        static let NavBarHeightSmallState: CGFloat = 44
+        /// Height of NavBar for Large state. Usually it's just 96.5 but if you have a custom font for the title, please make sure to edit this value since it changes the height for Large state of NavBar
+        static let NavBarHeightLargeState: CGFloat = 96.5
+    }
+    
+    var addButton = UIButton(type: .custom)
+    private func setupUI() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        
+        // Initial setup for image for Large NavBar state since the the screen always has Large NavBar once it gets opened
+        guard let navigationBar = self.navigationController?.navigationBar else { return }
+        navigationBar.addSubview(addButton)
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+        addButton.setImage(UIImage(named: "addBtn"), for: .normal)
+        addButton.addTarget(self, action: #selector(addBtnClicked(_:)), for: .touchUpInside)
+        
+        
+        NSLayoutConstraint.activate([
+            addButton.rightAnchor.constraint(equalTo: navigationBar.rightAnchor, constant: -Const.ImageRightMargin),
+            addButton.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: -Const.ImageBottomMarginForLargeState),
+            addButton.heightAnchor.constraint(equalToConstant: Const.ImageSizeForLargeState),
+            addButton.widthAnchor.constraint(equalTo: addButton.heightAnchor)
+            ])
+    }
 }
+
+
+
